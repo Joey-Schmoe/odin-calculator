df8d264533d63ecb3c8ccc6c1436a101dc67f95f
JoeySchmoe <narakuian19@gmail.com>
2022-03-14 14:09:40 -0600

    Write structure for adding event listeners

---
+++ javascript.js
@@ -0,0 +1,41 @@
+const calculatorDisplay = document.querySelector("#calculator-display");
+
+function addListeners() {
+    const buttons = document.querySelectorAll("button");
+
+    // 7
+
+    // 8
+
+    // 9
+
+    // Divide
+
+    // 4
+
+    // 5
+
+    // 6
+
+    // Multiply
+
+    // 1
+
+    // 2
+
+    // 3
+
+    // Subtract
+
+    // 0
+
+    // Decimal
+
+    // Equals
+
+    // Plus
+}
+
+function display() {
+    
+}
