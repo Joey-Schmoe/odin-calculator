3c9847e993370d59fce0c23b675a6e82423303cb
JoeySchmoe <narakuian19@gmail.com>
2022-03-17 13:30:18 -0600

    Entering a new number after resolving an operation begins a new num1 input

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@
 //    attempts to resolve the operation
 //   
 //    Pressing EQUALS should only do ANYTHING if user has entered all three inputs (num1, num2, operator)
+//
+//LOGIC
+//  
 
 const calculatorDisplay = document.querySelector("#display");
 const currentValueDisplay = document.querySelector('#testing');
@@ -12,6 +15,7 @@ let currentValue = "";
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+let lastPressed = "";
 
 addListeners();
 
@@ -59,48 +63,68 @@ function setOperation(operation, displayString) {
     updateCurrentValue("");
 }
 
+function inputNumber(num) {
+    if (lastPressed == "EQL") {
+        num1 = num;
+        currentValue = "";
+        displayValue = "";
+    }
+
+    display(num);
+    updateCurrentValue(num);
+    lastPressed = "NUM";
+
+}
+
 function addListeners() {
     const buttons = document.querySelectorAll("#calculator-buttons button");
 
     // 7
     buttons[0].addEventListener('click', () => {
-        display(7);
-        updateCurrentValue(7);
+        inputNumber(7);
+        // display(7);
+        // updateCurrentValue(7);
+        // lastPressed = "NUM";
     });
     // 8
     buttons[1].addEventListener('click', () => {
-        display(8);
-        updateCurrentValue(8);
+        inputNumber(8);
+        // display(8);
+        // updateCurrentValue(8);
+        // lastPressed = "NUM";
     });
     // 9
     buttons[2].addEventListener('click', () => {
-        display(9);
-        updateCurrentValue(9);
+        inputNumber(9);
+        // display(9);
+        // updateCurrentValue(9);
+        // lastPressed = "NUM";
     });
     // Divide
     buttons[3].addEventListener('click', () => {
-        // num1 = +currentValue;
-        // currentValue = "";
-        // operator = "DIVIDE";
-        // display(" / ");
-        // updateCurrentValue("");
-
         setOperation("DIVIDE", "/");
+        lastPressed = "OPR";
     });
     // 4
     buttons[4].addEventListener('click', () => {
-        display(4);
-        updateCurrentValue(4);
+        inputNumber(4);
+        // display(4);
+        // updateCurrentValue(4);
+        // lastPressed = "NUM";
     });
     // 5
     buttons[5].addEventListener('click', () => {
-        display(5);
-        updateCurrentValue(5);
+        inputNumber(5);
+        // display(5);
+        // updateCurrentValue(5);
+        // lastPressed = "NUM";
     });
     // 6
     buttons[6].addEventListener('click', () => {
-        display(6);
-        updateCurrentValue(6);
+        inputNumber(6);
+        // display(6);
+        // updateCurrentValue(6);
+        // lastPressed = "NUM";
     });
     // Multiply
     buttons[7].addEventListener('click', () => {
@@ -111,21 +135,29 @@ function addListeners() {
         // updateCurrentValue("");
 
         setOperation("MULTIPLY", "x");
+        lastPressed = "OPR";
+
     });
     // 1
     buttons[8].addEventListener('click', () => {
-        display(1);
-        updateCurrentValue(1);
+        inputNumber(1);
+        // display(1);
+        // updateCurrentValue(1);
+        // lastPressed = "NUM";
     });
     // 2
     buttons[9].addEventListener('click', () => {
-        display(2);
-        updateCurrentValue(2);
+        inputNumber(2);
+        // display(2);
+        // updateCurrentValue(2);
+        // lastPressed = "NUM";
     });
     // 3
     buttons[10].addEventListener('click', () => {
-        display(3);
-        updateCurrentValue(3);
+        inputNumber(3);
+        // display(3);
+        // updateCurrentValue(3);
+        // lastPressed = "NUM";
     });
     // Subtract
     buttons[11].addEventListener('click', () => {
@@ -136,12 +168,16 @@ function addListeners() {
         // updateCurrentValue("");
 
         setOperation("SUBTRACT", "-");
+        lastPressed = "OPR";
+
     });
     // 0
     buttons[12].addEventListener('click', () => {
-        display("0");
-        //0 must be added as a string, otherwise multiple 0's in a row doesn't work
-        updateCurrentValue("0");
+        inputNumber("0");
+        // display("0");
+        // //0 must be added as a string, otherwise multiple 0's in a row doesn't work
+        // updateCurrentValue("0");
+        // lastPressed = "NUM";
     });
     // Decimal
     buttons[13].addEventListener('click', () => {
@@ -150,30 +186,32 @@ function addListeners() {
     });
     // Equals
     buttons[14].addEventListener('click', () => {
-        if (num1 != 0 && currentValue != "") {
+        if (lastPressed == "NUM" && num1 != 0) {
             num2 = +currentValue;
             switch (operator) {
                 case "ADD":
-                    operate(add);
+                    num1 = operate(add);
                     break;
     
                 case "SUBTRACT":
-                    operate(subtract);
+                    num1 = operate(subtract);
                     break;
     
                 case "MULTIPLY":
-                    operate(multiply);
+                    num1 = operate(multiply);
                     break;
     
                 case "DIVIDE":
-                    operate(divide);
+                    num1 = operate(divide);
                     break;
             }
-            num1 = 0;
             num2 = 0;
             currentValue = "";
             displayValue = "";
             operator = "";
+            lastPressed = "EQL";
+            updateCurrentValue(num1);
+            display(num1);
         } else {
 
         }
@@ -190,6 +228,8 @@ function addListeners() {
         // updateCurrentValue("");
 
         setOperation("ADD", "+");
+        lastPressed = "OPR";
+
     });
 
     document.querySelector("#button-clear").addEventListener('click', reset);
